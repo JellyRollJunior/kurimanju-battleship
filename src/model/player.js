98bc44798ea23cae2082c7f3ddef195b12471498
Brandon Lin <brandonlin9716@gmail.com>
2024-11-20 14:29:24 -0800

    feat: add gameboard to player

--- src/model/player.js
+++ src/model/player.js
@@ -1,7 +1,9 @@
+import { gameboard } from './gameboard.js';
 export { player };
 
 const player = (isHuman = true) => {
     const isHumanPlayer = isHuman;
+    const board = gameboard();
 
-    return { isHuman : isHumanPlayer };
+    return { isHuman: isHumanPlayer, board };
 };
--- src/model/tests/player.test.js
+++ src/model/tests/player.test.js
@@ -1,18 +1,23 @@
-import { player } from './../player.js'
+import { player } from './../player.js';
 
 describe('The player object', () => {
     test('Verify player object exists', () => {
         const playerOne = player();
         expect(playerOne).not.toBeUndefined();
-    })
+    });
 
     test('Verify can create human player', () => {
         const playerOne = player(true);
         expect(playerOne.isHuman).toBe(true);
-    })
+    });
 
     test('Verify can create non human player', () => {
         const playerOne = player(false);
         expect(playerOne.isHuman).toBe(false);
-    })
-})
+    });
+
+    test('Verify player has gameboard', () => {
+        const playerOne = player(true);
+        expect(() => playerOne.board.getBoard()).not.toThrowError(Error);
+    });
+});
