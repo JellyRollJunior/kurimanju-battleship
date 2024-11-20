164748e008eb3bb1bde03cfb1843a7426334f54a
Brandon Lin <brandonlin9716@gmail.com>
2024-11-19 17:14:04 -0800

    feat: Implement board of cells

--- src/model/gameboard.js
+++ src/model/gameboard.js
@@ -1,5 +1,25 @@
 export { gameboard };
 
+const cell = () => {
+    const hitStatus = false;
+    const isHit = () => hitStatus;
+    const hit = () => hitStatus = true;
+
+    return { isHit, hit }
+}
+
 const gameboard = () => {
-    return {};
+    const length = 10;
+
+    const createGameBoard = (length) => {
+        const newBoard = [];
+        for (let i = 0; i < length; i++) {
+            newBoard.push(Array(length).fill(cell()));
+        }
+        return newBoard;
+    };
+    let board = createGameBoard(length);
+    const getBoard = () => board;
+
+    return { length, getBoard };
 };
--- src/model/tests/gameboard.test.js
+++ src/model/tests/gameboard.test.js
@@ -1,8 +1,14 @@
-import { gameboard } from './../gameboard.js'
+import { gameboard } from './../gameboard.js';
 
 describe('The gameboard object', () => {
     test('Verify gameboard object exists', () => {
         const board = gameboard();
         expect(board).not.toBeUndefined();
-    })
-})
+    });
+
+    test('Verify gameboard can return board state', () => {
+        const board = gameboard();
+        expect(board.getBoard().length).toStrictEqual(board.length);
+        expect(board.getBoard()[0].length).toStrictEqual(board.length);
+    });
+});
