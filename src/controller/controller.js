1b5978c97efa9eb4c32257dcfd0c9f019f785503
Brandon Lin <brandonlin9716@gmail.com>
2024-11-20 16:29:42 -0800

    feat: initialize two players on start

---
+++ src/controller/controller.js
@@ -0,0 +1,21 @@
+import { player } from './../model/player.js';
+export { controller };
+
+const controller = () => {
+
+    const init = () => {
+        // init boards and place ships on board
+        const playerOne = player(true);
+        const playerTwo = player(false);
+
+        playerOne.board.placeShip(0, 0, 2, true);
+        playerOne.board.placeShip(1, 0, 2, false);
+        playerOne.board.prettyPrintBoard();
+
+        playerTwo.board.placeShip(0, 0, 2, true);
+        playerTwo.board.placeShip(1, 0, 2, false);
+        playerTwo.board.prettyPrintBoard();
+    };
+    
+    return { init };
+};
