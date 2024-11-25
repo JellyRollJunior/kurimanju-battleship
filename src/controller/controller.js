df73c05abb4708d083b2b94fa0f3b9932c403099
Brandon Lin <brandonlin9716@gmail.com>
2024-11-25 15:36:20 -0800

    feat: play round by clicking board cell

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,5 +1,5 @@
 import { gameController } from '../model/game-controller.js';
-import { renderPlayerOneBoard, renderPlayerTwoBoard, clearBoards } from './../view/view.js';
+import { renderPlayerOneBoard, renderPlayerTwoBoard } from './../view/view.js';
 export { init };
 
 const handleBoardClick = (game) => {
@@ -7,8 +7,11 @@ const handleBoardClick = (game) => {
     playerOneBoard.addEventListener('click', (event) => {
         const target = event.target;
         if (target.tagName == 'TD') {
-            console.log(`col: ${target.dataset.column}`);
-            console.log(`row: ${target.dataset.row}`);
+            const x = target.dataset.column;
+            const y = target.dataset.row;
+            game.playRound(x, y);
+            renderPlayerOneBoard(game.getChallenger().board.getBoard());
+            renderPlayerTwoBoard(game.getRival().board.getBoard());
         }
     })
 }
@@ -19,17 +22,10 @@ const init = () => {
     game.getChallenger().board.usePresetShipLayout();
     game.getRival().board.usePresetShipLayout();
 
-    // play game
-    game.playRound(0, 0);
-    game.playRound(0, 1);
-    game.playRound(0, 3);
-    game.playRound(3, 1);
-    game.playRound(1, 3);
-
     // render board
     renderPlayerOneBoard(game.getChallenger().board.getBoard());
     renderPlayerTwoBoard(game.getRival().board.getBoard());
 
     // event handlers
-    handleBoardClick();
+    handleBoardClick(game);
 };
